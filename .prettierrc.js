e7eda173c93a9f638de1a45a17a2d92037bbfc93
xi.liu <xi.liu@aispeech.com>
2019-11-21 17:57:22 +0800

    add vue-cli file

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'none',
+    semi: false,
+    tabWidth: 4,
+    bracketSpacing: false,
+    jsxBracketSameLine: true
+}
